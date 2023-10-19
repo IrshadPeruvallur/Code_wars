48aeb4e437b29480a8f8ccb8277b5391ae47c69e
IrshadPeruvallur <irshadpukayoor@gmail.com>
2023-10-19 10:17:46 +0530

    flick_switch

---
+++ flick_witch.dart
@@ -0,0 +1,5 @@
+// Create a function that always returns true for every item in a given list. However, if an element is the word "flick", switch to always returning the opposite boolean value.
+
+// Examples
+// ["codewars", "flick", "code", "wars"] ➞ [true, false, false, false
+
--- smallest_integer.dart
+++ smallest_integer.dart
@@ -3,3 +3,14 @@
 // Given [34, 15, 88, 2] your solution will return 2
 // Given [34, -345, -1, 100] your solution will return -345
 
+void main(List<String> args) {
+int findSmallestInt(List<int> arr) {
+  int smallest = arr[0];
+  for (int x in arr){
+  if(x<smallest){
+    smallest = x;
+  }
+}
+  return smallest;
+}
+}
